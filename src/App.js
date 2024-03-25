6c09a89e534ca3afc70c1e70897c114f6790c5d4
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2024-03-26 00:20:44 +0100

    Implemented ui for cart page and made available store data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import NavBar from './Component/NavBar'
+import CartContainer from './Component/CartContainer'
 const App = () => {
   return (
     <main>
       <NavBar />
+      <CartContainer />
     </main>
   )
 }
