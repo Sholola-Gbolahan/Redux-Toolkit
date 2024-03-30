7ecf1554c3c092587dd2e9a907beedff64b65e7a
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2024-03-30 17:54:52 +0100

    fetched cart API data using createAsyncThunk

--- src/App.js
+++ src/App.js
@@ -2,21 +2,34 @@ import React from 'react'
 import NavBar from './Component/NavBar'
 import CartContainer from './Component/CartContainer'
 import { useDispatch, useSelector } from 'react-redux'
-import { calculateTotals } from './features/cart/cartSplice'
+import { calculateTotals, getCartItems } from './features/cart/cartSplice'
 import { useEffect } from 'react'
 import Modal from './Component/Modal'
 
 
 
 const App = () => {
-  const { cartItems} = useSelector( (store) => store.cart)
+  const { cartItems, isLoading} = useSelector( (store) => store.cart)
 // this import the value of isOpen from the modal Store
   const { isOpen} = useSelector( (store) => store.modal)
-  const dispatch = useDispatch();
+  const dispatch = useDispatch(); 
 
   useEffect( () => {
     dispatch(calculateTotals());
   }, [cartItems] )
+
+  useEffect(() => {
+      dispatch(getCartItems())
+  }, []);
+
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+          <h2>Loading.......</h2>
+      </div>
+    )
+  }
 
   return (
     <main>
